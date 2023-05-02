9fa1ad9f22a1a3d7c185946744310b36d5046bcd
Nam Khoa <khoa.pham28042002@hcmut.edu.vn>
2023-05-02 15:12:05 +0700

    create modal detail and update description for lamp

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,43 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+# from flask_mqtt import Mqtt
 from flask_cors import CORS, cross_origin
+from Adafruit_IO import MQTTClient
+
 app = Flask(__name__)
 cors = CORS(app)
 
-# app.config["SECRET_KEY"] = 
+# Config Mongodb
 app.config["MONGO_URI"] = "mongodb+srv://winnawa:namkhoapro2804@cluster0.8p7d3.mongodb.net/IoT_control?retryWrites=true&w=majority"
 
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
+# Config MQTT
+app.config['MQTT_CLIENT_ID'] = 'aasasdd'
+username = 'lnminhthu1505'
+key = 'lnminhthu1505/feeds/smart-home.rgb'
+# f"mqtts://{username}:{key}@io.adafruit.com" 
+app.config['MQTT_BROKER_URL'] =  "io.adafruit.com"# use the free broker from HIVEMQ
+app.config['MQTT_BROKER_PORT'] = 1883 # default port for non-tls connection
+app.config['MQTT_USERNAME'] = 'lnminhthu1505' # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = key # set the password here if the broker demands authentication
+app.config['MQTT_KEEPALIVE'] = 5 # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_TLS_ENABLED'] = False # set TLS to disabled for testing purposes
+
+# mqtt = Mqtt(app)
+
+
+
+
+
+
+# Set to your Adafruit IO key.
+# Remember, your key is a secret,
+# so make sure not to publish it when you publish this code!
+ADAFRUIT_IO_KEY = 'aio_teLx19UtogwB52wolFfreAFt5UVd'
+
+# Set to your Adafruit IO username.
+# (go to https://accounts.adafruit.com to find your username)
+ADAFRUIT_IO_USERNAME = 'lnminhthu1505'
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from crypt import methods
-import re
-from urllib import request, response
 from flask import request
 from src.error import APIError, BadRequestError, ErrorMapper, NotFoundError, UnauthorizedError
-from src import db, app
+from src import db, app, client
+import random
+import sys
+import time
 
 @app.route('/')
 def hello_world():
@@ -103,10 +103,10 @@ def updateThermoDetail(id):
     print('here')
     try:
         data = request.get_json()
-        newName = data['name']
-        newImageUrl = data['imageUrl']
+        newName = None if 'name' not in data else data['name']
+        newImageUrl = None if 'imageUrl' not in data else data['imageUrl']
 
-        if not(newName and newImageUrl):
+        if not(newName or newImageUrl):
             raise BadRequestError("All fields are empty")
 
         print(id)
@@ -143,3 +143,104 @@ def updateThermoDetail(id):
     except APIError as error:
         return ErrorMapper.mappError(error)
    
+
+@app.route("/lamps/", methods=["GET"])
+def getLamps():
+    try:
+        existeLamps = db.Lamp.find({})
+
+        return {
+            "message": "update successfully",
+            "data": [   {
+                            "lamp_id": element["lamp_id"],
+                            "status": element["status"],
+                            "name": element["name"],
+                            "imageUrl": element["imageUrl"]
+                        } for element in existeLamps]
+        }
+        
+    except APIError as error:
+        return ErrorMapper.mappError(error) 
+
+@app.route("/lamps/<id>", methods=["PATCH"])
+def updateLampDetail(id):
+    print('here')
+    try:
+        data = request.get_json()
+        newName = None if 'name' not in data else data['name']
+        newImageUrl = None if 'imageUrl' not in data else data['imageUrl']
+        newStatus = None if 'status' not in data else data['status'] 
+        print(newName,newImageUrl,newStatus)
+
+        if not(newName or newImageUrl or newStatus):
+            raise BadRequestError("All fields are empty")
+
+        existedLamp = db.Lamp.find_one(
+            {
+                "lamp_id": id
+            })
+        
+        if not(existedLamp):
+            raise NotFoundError("lamp not found")
+        
+        if existedLamp:
+            if newName and existedLamp['name'] != newName:
+                existedLamp['name'] = newName
+            if newImageUrl and existedLamp['imageUrl'] != newImageUrl:
+                existedLamp['imageUrl'] = newImageUrl
+            if newStatus and existedLamp['status'] != newStatus:
+                existedLamp['status'] = newStatus
+
+
+            db.Lamp.find_one_and_update(
+            {
+                "lamp_id":id
+            }, 
+            {
+                "$set":{
+                            "name": existedLamp['name'],
+                            "imageUrl" : existedLamp['imageUrl'],
+                            "status":existedLamp['status']
+                        }
+            })
+
+            return {
+                "message": "update successfully",
+            }
+        
+    except APIError as error:
+        return ErrorMapper.mappError(error)
+   
+
+
+
+
+
+
+def connected(client):
+    # Connected function will be called when the client is connected to Adafruit IO.
+    # This is a good place to subscribe to feed changes.  The client parameter
+    # passed to this function is the Adafruit IO MQTT client so you can make
+    # calls against it easily.
+    print('Connected to Adafruit IO!  Listening for DemoFeed changes...')
+    # Subscribe to changes on a feed named DemoFeed.
+    client.subscribe('temp')
+
+def disconnected(client):
+    # Disconnected function will be called when the client disconnects.
+    print('Disconnected from Adafruit IO!')
+    sys.exit(1)
+
+def message(client, feed_id, payload):
+    # Message function will be called when a subscribed feed has a new value.
+    # The feed_id parameter identifies the feed, and the payload parameter has
+    # the new value.
+    print('Feed {0} received new value: {1}'.format(feed_id, payload))
+
+# Setup the callback functions defined above.
+client.on_connect    = connected
+client.on_disconnect = disconnected
+client.on_message    = message
+
+# Connect to the Adafruit IO server.
+client.connect()
